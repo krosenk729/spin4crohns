fdb2b8e25b48a196252ef4b57f85ff5c99d663c5
krosenk729 <krosenk729@gmail.com>
2019-05-11 06:16:20 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Static Files
+app.use('/assets', express.static('assets'))
+
+// Home Page
+app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
